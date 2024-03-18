7b86e063cf8bcf127008f802319414e996a4601a
LetsJhUnity <163387942+LetsJhUnity@users.noreply.github.com>
2024-03-18 14:36:32 +0900

    Singleton 패턴

--- Program.cs
+++ Program.cs
@@ -2,64 +2,39 @@
 
 class Program
 {
-    class Parent
+    //객체 한개 존재
+    class Singleton
     {
-        public virtual void Pay()
+        private Singleton()
         {
-            Console.WriteLine("지불한다.");
         }
-    }
 
-    class Child1 : Parent
-    {
-        public override void Pay()
+        public static Singleton GetInstance()
         {
-            Console.WriteLine("대신 지불한다.");
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
         }
-    }
 
-    class Child2 : Parent
-    {
-        public void Run()
+        private static Singleton? instance;
+
+        public void Draw()
         {
-            Console.WriteLine("도망간다.");
+
         }
     }
 
     static void Main(string[] args)
     {
-        List<Parent> lists = new List<Parent>();
-        lists.Add(new Child1());
-        lists.Add(new Parent());
-        lists.Add(new Child2());
-
-        for(int i = 0; i < lists.Count; i++) 
-        {
-            //lists[i].Pay();
-            //Reflection
-            //if( lists[i].GetType() == typeof(Child2) )
-            //{
-            //    ((Child2)lists[i]).Run();
-            //}
-            //다운캐스팅
-            Child2 child2 = lists[i] as Child2;
-            if (child2 != null)
-            {
-                child2.Run();
-                ((Parent)child2).Pay();
-            }
-            else
-            {
-                Console.WriteLine("Child2가 아님");
-            }
-
-        }
+        Singleton.GetInstance().Draw();
 
         //        Engine engine = new Engine();
 
         //        engine.Init();
         //        engine.LoadScene("level01.map");
-        ////        engine.Run();
+        //        engine.Run();
         //        engine.Term();
     }
 }
