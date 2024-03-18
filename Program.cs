5fc8f7dbd38409768b4fbbeec2782a077365866d
LetsJhUnity <163387942+LetsJhUnity@users.noreply.github.com>
2024-03-18 14:23:42 +0900

    down castring

--- Program.cs
+++ Program.cs
@@ -4,45 +4,56 @@ class Program
 {
     class Parent
     {
-
+        public virtual void Pay()
+        {
+            Console.WriteLine("지불한다.");
+        }
     }
 
     class Child1 : Parent
     {
-        public Child1() { }
-        public Child1(int a) { }
-
-        public int number;
-
-        public int Gold { get; set; }
+        public override void Pay()
+        {
+            Console.WriteLine("대신 지불한다.");
+        }
     }
 
     class Child2 : Parent
     {
+        public void Run()
+        {
+            Console.WriteLine("도망간다.");
+        }
     }
 
     static void Main(string[] args)
     {
-        //GetConstructors()
-        //GetFields()
-        //GetInterfaces()
-        //GetMembers()
-        //GetMethods()
-        //GetProperties()
-        Child1 c = new Child1();
+        List<Parent> lists = new List<Parent>();
+        lists.Add(new Child1());
+        lists.Add(new Parent());
+        lists.Add(new Child2());
 
-        FieldInfo[] datas = c.GetType().GetFields();
-        foreach (FieldInfo d in datas)
+        for(int i = 0; i < lists.Count; i++) 
         {
-           Console.WriteLine(d.Name);
-        }
+            //lists[i].Pay();
+            //Reflection
+            //if( lists[i].GetType() == typeof(Child2) )
+            //{
+            //    ((Child2)lists[i]).Run();
+            //}
+            //다운캐스팅
+            Child2 child2 = lists[i] as Child2;
+            if (child2 != null)
+            {
+                child2.Run();
+                ((Parent)child2).Pay();
+            }
+            else
+            {
+                Console.WriteLine("Child2가 아님");
+            }
 
-        PropertyInfo[] datas2 = c.GetType().GetProperties();
-        foreach (PropertyInfo d in datas2)
-        {
-            Console.WriteLine(d.Name);
         }
-
 
         //        Engine engine = new Engine();
 
